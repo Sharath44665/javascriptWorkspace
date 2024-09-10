5651f1bd6764424790feb4ad779cd605d3cbb162
Sharath44665 <sharathwelcomesyouin@gmail.com>
2024-09-10 22:46:39 +0530

    work on ES2022

---
+++ javascriptNuggets/22_ES2022/app.js
@@ -0,0 +1,18 @@
+const scores = [99, 100, 95]
+const lastScore = scores[scores.length -1]
+console.log(lastScore) // 95
+
+
+const anotherWayLast = scores.at(-1) // es2022          
+console.log(anotherWayLast) // 95
+
+
+const name = "Sharathchandra"
+console.log(name.at(0)) // S
+
+const resp = await fetch('https://www.course-api.com/react-tabs-project');
+const data = await resp.json()
+console.log(data)
+
+
+
